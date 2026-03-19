28c526f4bbbeec42f3d1627ac6ebabf119a60270
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2026-03-19 23:16:45 +0500

    Сменил титульник

--- PVE-ASDaC-BASH.sh
+++ PVE-ASDaC-BASH.sh
@@ -5,7 +5,7 @@ trap ex INT
 
 # Запуск:               sh='PVE-ASDaC-BASH.sh';curl -sOLH 'Cache-Control: no-cache' "https://raw.githubusercontent.com/PavelAF/PVE-ASDaC-BASH/main/$sh"&&chmod +x $sh&&./$sh;rm -f $sh
 
-echo $'\nProxmox VE Automatic stand deployment and configuration script by AF\n' >> /dev/tty
+echo $'\nProxmox VE Automatic stand deployment and configuration script by PavelAF, modified by NiKeNO1540\n' >> /dev/tty
 
 ############################# -= Конфигурация =- #############################
 
